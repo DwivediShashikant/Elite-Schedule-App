34504af663c8b06b9c1c7789d1f271c35bb94d11
DwivediShashikant <dwivedi.sk@outlook.com>
2018-01-30 10:46:25 +0530

    add different pages and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import {
+   GamePage,
+   MyTeamsPage,
+   TeamDetailPage,
+   TeamsPage,
+   TournamentsPage
+ } from '../pages/pages'
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    GamePage,
+    MyTeamsPage,
+    TeamDetailPage,
+    TeamsPage,
+    TournamentsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    GamePage,
+    MyTeamsPage,
+    TeamDetailPage,
+    TeamsPage,
+    TournamentsPage
   ],
   providers: [
     StatusBar,
